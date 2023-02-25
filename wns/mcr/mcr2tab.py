e695c44966fd0024acfcecf6c3b687a53212eed1
Eric Kafe <kafe.eric@gmail.com>
2023-02-25 07:05:59 +0100

    Filter to avoid duplicates

--- wns/mcr/mcr2tab.py
+++ wns/mcr/mcr2tab.py
@@ -14,6 +14,8 @@ import sys
 import codecs
 import re
 
+from collections import defaultdict
+
 #wndata= "/home/bond/svn/wn-msa/tab/"
 wndata = 'mcr'
 
@@ -38,7 +40,7 @@ endspace = re.compile(" $")
 
 logf = codecs.open("MCR-log.txt", "w", "utf-8" )
 
-def normalize(offset, att, text0):
+def normalize(text0, context='string', where=None):
     """Normalize space around punctuation and at end of string"""
     text = text0.replace('??', ' ').replace('_', ' ')
     while spacepunct.search(text):
@@ -49,9 +51,8 @@ def normalize(offset, att, text0):
     if e:
         text = text[:e.start()]
     if text != text0:
-        logf.write(f"{offset}, normalize space in {att}: '{text0}'\n -> '{text}'\n")
+        logf.write(f"{where}, normalize space in {context}: '{text0}'\n -> '{text}'\n")
     return text
-
 
 for wnlang in ["cat", "glg", "spa", "eus"]:
     #
@@ -67,6 +68,8 @@ for wnlang in ["cat", "glg", "spa", "eus"]:
     f = codecs.open("%s/%s/%sWN/wei_%s-30_variant.tsv" % (wns_dir, mcr_version, wnlang, wnlang),
     "rb", "utf-8")
 
+    tmpdict = defaultdict(set)    # Filter, to avoid duplicates
+
     for l in f:
 #        (lemma, rank, offset, pos, conf, src, note)
         attrs = l.strip().split("\t")
@@ -77,8 +80,12 @@ for wnlang in ["cat", "glg", "spa", "eus"]:
             if "," in lemma and not commafloat.match(lemma):
                 logf.write(f"{offset}: truncating lemma '{lemma}'\n")
                 lemma=lemma[:lemma.find(",")]  ### discard anything after a comma
-            om.write("%s\tlemma\t%s\n" % (synset, lemma))
+            tmpdict[synset].add(lemma)
     f.close()
+
+    for synset in sorted(tmpdict.keys()):
+        for lemma in tmpdict[synset]:
+            om.write("%s\t%s:lemma\t%s\n" % (synset, wnlang, lemma))
 
 
     ### FIXED also add gloss and examples:
@@ -86,14 +93,20 @@ for wnlang in ["cat", "glg", "spa", "eus"]:
     f = codecs.open("%s/%s/%sWN/wei_%s-30_synset.tsv" % (wns_dir, mcr_version, wnlang, wnlang),
     "rb", "utf-8")
 
+    tmpdict = defaultdict(set)
+
     for l in f:
         attrs = l.strip().split("\t")
         if len(attrs)>6 and attrs[6] not in ["", "0", "None","NULL"]:
             offset = attrs[0]
             synset = offset[-10:]
             if int(synset[0]) < 8:  ### ignore new synsets
-                om.write("%s\t%s:def\t0\t%s\n" % (synset, wnlang, normalize(offset, 'definition', attrs[6])))
+                tmpdict[synset].add(normalize(attrs[6], 'definition', offset))
     f.close()
+
+    for synset in sorted(tmpdict.keys()):
+        for definition in tmpdict[synset]:
+            om.write("%s\t%s:def\t0\t%s\n" % (synset, wnlang, definition))
 
 
     ### MCR links the examples to specific lemma ranks, which are not yet supported by OMW.
@@ -102,14 +115,21 @@ for wnlang in ["cat", "glg", "spa", "eus"]:
     f = codecs.open("%s/%s/%sWN/wei_%s-30_examples.tsv" % (wns_dir, mcr_version, wnlang, wnlang),
     "rb", "utf-8")
 
+    tmpdict = defaultdict(set)
+
     for l in f:
         attrs = l.strip().split("\t")
         if len(attrs)>4 and attrs[2] not in ["", "0", "None","NULL"]:
             offset = attrs[4]
             synset = offset[-10:]
             if int(synset[0]) < 8:  ### ignore new synsets
-                om.write("%s\t%s:exe\t0\t%s\n" % (synset, wnlang, normalize(offset, 'example', attrs[2])))
+                tmpdict[synset].add(normalize(attrs[2], 'example', offset))
     f.close()
+
+    for synset in sorted(tmpdict.keys()):
+        for example in tmpdict[synset]:
+            om.write("%s\t%s:exe\t0\t%s\n" % (synset, wnlang, example))
+
 
     om.close()
 logf.close()
