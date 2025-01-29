19944d56affd8e9a24c5de55596ffbb3b1958237
Michael Wayne Goodman <goodman.m.w@gmail.com>
2025-01-29 11:49:02 -0800

    Update tsv-duplicates.py
    * Report exact and normalized duplicates separately
    * Add support for polysemy thresholds

--- scripts/tsv-duplicates.py
+++ scripts/tsv-duplicates.py
@@ -1,114 +1,168 @@
 import argparse
-from collections import defaultdict
-import csv
 import logging
-from pathlib import Path
 import sys
-import tempfile
-from typing import Tuple
+from functools import partial
+from itertools import groupby
+from pathlib import Path
+from typing import Callable
 from unicodedata import normalize, combining
 
+from .util import load_tsv, strip_quotes
 
-logger = logging.getLogger('tsv-duplicates')
-
-Entry = Tuple[str, str, str]
-
-
-def normalize_lemma(s: str, args: argparse.Namespace) -> str:
-    norm = s
-    if args.ignore_case:
-        norm = norm.lower()
-    if args.underscore:
-        norm = norm.replace('_', ' ')
-    if args.diacritics:
-        norm = ''.join(c for c in normalize('NFKD', norm) if not combining(c))
-    return norm
+logger = logging.getLogger("tsv-duplicates")
 
 
-def load_tsv(path: Path) -> tuple[str, list[Entry]]:
-    with path.open(newline='') as csvfile:
-        reader = csv.reader(csvfile, dialect='excel-tab', quoting=csv.QUOTE_NONE)
-        header = next(reader)
-        entries: list[Entry] = []
-        for row in reader:
-            if not row or row[0].startswith('#'):
-                continue
-            if row[1].endswith("lemma"):
-                assert len(row) == 3, f"unexpected number of columns: {row}"
-                entries.append((row[0], row[1], row[2]))
-    return header, entries
+def make_normalizer(args: argparse.Namespace) -> Callable[[str], str]:
+
+    def normalize_lemma(s: str) -> str:
+        norm = s
+        if args.ignore_case:
+            norm = norm.lower()
+        if args.underscore:
+            norm = norm.replace("_", " ")
+        if args.diacritics:
+            norm = "".join(c for c in normalize("NFKD", norm) if not combining(c))
+        if args.quotes:
+            norm = strip_quotes(norm.strip())
+        return norm
+
+    return normalize_lemma
 
 
-def check_duplicates(path: Path, args: argparse.Namespace) -> int:
-    header, entries = load_tsv(path)
-    seen: dict[str, dict[str, list[str]]] = defaultdict(
-        lambda: defaultdict(list)
-    )
-    for off_pos, _, lemma in entries:
-        seen[off_pos][normalize_lemma(lemma, args)].append(lemma)
+def check_duplicates(
+    offset_pos: str,
+    lemmas: list[str],
+    normalizer: Callable[[str], str],
+    label: str,
+    verbose: bool,
+) -> int:
+    _sorted = sorted(lemmas, key=normalizer)
+    count = 0
+    for _, grp in groupby(_sorted, key=normalizer):
+        dupes = list(grp)
+        if len(dupes) > 1:
+            if verbose:
+                print(f"\t{offset_pos}\texact\t{'; '.join(dupes)}")
+            count += len(dupes) - 1
+    return count
 
-    synset_count = 0
-    lemma_count = 0
-    for off_pos, lemma_groups in seen.items():
-        for norm, lemmas in lemma_groups.items():
-            if len(lemmas) > 1:
-                synset_count += 1
-                lemma_count += len(lemmas)
-                logger.warning(
-                    f"duplicate of {off_pos}: " + ", ".join(map(repr, lemmas))
-                )
 
-    return synset_count, lemma_count
+def check_polysemy(
+    data: dict[str, list[str]],
+    polysemy_threshold: int,
+    verbose: bool,
+) -> int:
+    # map lemmas to synsets
+    _data: dict[str, list[str]] = {}
+    for offset_pos, lemmas in data.items():
+        for lemma in set(lemmas):
+            _data.setdefault(lemma, []).append(offset_pos)
+    polysem_count = 0
+    for lemma, offsets in _data.items():
+        if len(offsets) >= polysemy_threshold:
+            if verbose:
+                print(f"\t{lemma}\tpolysem\t{'; '.join(offsets)}")
+            polysem_count += 1
+    return polysem_count
+
+
+def load_data(path: Path) -> dict[str, list[str]]:
+    data: dict[str, list[str]] = {}
+    _, rows = load_tsv(path)
+    for row in rows:
+        if not row.is_lemma():
+            continue
+        data.setdefault(row.offset_pos, []).append(row.text)
+    return data
 
 
 def main(args: argparse.Namespace) -> int:
     retcode = 0
 
-    synset_total = lemma_total = 0
-    for path in args.TSV:
-        synset_count, lemma_count = check_duplicates(path, args)
-        if synset_count or lemma_count:
-            print(
-                f"{path.name} duplicates"
-                f"\t{synset_count} synsets"
-                f"\t{lemma_count} lemmas"
-            )
-            synset_total += synset_count
-            lemma_total += lemma_count
-            retcode = 1
+    check_exact = partial(
+        check_duplicates,
+        normalizer=str,
+        label="exact",
+        verbose=args.verbose,
+    )
+    check_normalized = partial(
+        check_duplicates,
+        normalizer=make_normalizer(args),
+        label="".join([
+            "i" if args.ignore_case else "",
+            "u" if args.underscore else "",
+            "d" if args.diacritics else "",
+            "q" if args.quotes else "",
+        ]),
+        verbose=args.verbose,
+    )
 
-    print(f"total duplicates\t{synset_total} synsets\t{lemma_total} lemmas")
+    for path in args.TSV:
+        print(f"Checking for duplicates in {path}")
+        data = load_data(path)
+
+        synsets: set[str] = set()
+        lemma_count = 0
+        for offset_pos, lemmas in data.items():
+            # exact duplicates
+            if cnt := check_exact(offset_pos, lemmas):
+                synsets.add(offset_pos)
+                lemma_count += cnt
+            # normalized duplicates minus exact duplicates
+            if cnt := check_normalized(offset_pos, set(lemmas)):
+                synsets.add(offset_pos)
+                lemma_count += cnt
+        print(f"  Synsets with redundant lemmas: {len(synsets)}")
+        print(f"  Total count of redundant lemmas: {lemma_count}")
+
+        if args.polysemy_threshold:
+            polysem_count = check_polysemy(data, args.polysemy_threshold, args.verbose)
+            print(f"  Lemmas with > {args.polysemy_threshold} senses: {polysem_count}")
 
     return retcode
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("TSV", nargs='+', type=Path, help="path to TSV file")
-parser.add_argument(
-    '-v',
-    '--verbose',
-    action='store_true',
-    help='log warnings for each item',
-)
-parser.add_argument(
-    '-i',
-    '--ignore-case',
-    action='store_true',
-    help='ignore case distinctions in lemmas',
-)
-parser.add_argument(
-    '-u',
-    '--underscore',
-    action='store_true',
-    help='treat underscores as spaces in lemmas',
-)
-parser.add_argument(
-    '-d',
-    '--diacritics',
-    action='store_true',
-    help='ignore diacritics with NFKD normalization',
-)
-args = parser.parse_args()
-logging.basicConfig(level=logging.WARNING if args.verbose else logging.ERROR)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("TSV", nargs="+", type=Path, help="path to TSV file")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="log warnings for each item",
+    )
+    parser.add_argument(
+        "-i",
+        "--ignore-case",
+        action="store_true",
+        help="ignore case distinctions in lemmas",
+    )
+    parser.add_argument(
+        "-u",
+        "--underscore",
+        action="store_true",
+        help="treat underscores as spaces in lemmas",
+    )
+    parser.add_argument(
+        "-d",
+        "--diacritics",
+        action="store_true",
+        help="ignore diacritics with NFKD normalization",
+    )
+    parser.add_argument(
+        "-q",
+        "--quotes",
+        action="store_true",
+        help="strip quote pairs from start/end of lemmas",
+    )
+    parser.add_argument(
+        "-p",
+        "--polysemy-threshold",
+        type=int,
+        metavar="N",
+        help="report lemmas that are used in N (>=2) or more synsets",
+    )
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.WARNING if args.verbose else logging.ERROR)
 
-sys.exit(main(args))
+    sys.exit(main(args))
